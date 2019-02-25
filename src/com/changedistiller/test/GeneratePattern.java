4899c1632daa3da49fee8137a9abb3886aaeae14
NEU_Zhangy <alice7neusoft1207@gmail.com>
2019-02-25 13:59:31 -0500

    refactor GeneratePattern

--- src/com/changedistiller/test/GeneratePattern.java
+++ src/com/changedistiller/test/GeneratePattern.java
@@ -1,9 +1,20 @@
 package com.changedistiller.test;
 
+import ch.uzh.ifi.seal.changedistiller.ChangeDistiller;
+import ch.uzh.ifi.seal.changedistiller.distilling.FileDistiller;
+import ch.uzh.ifi.seal.changedistiller.model.classifiers.SourceRange;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Delete;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Insert;
 import ch.uzh.ifi.seal.changedistiller.model.entities.SourceCodeChange;
+import ch.uzh.ifi.seal.changedistiller.model.entities.Update;
+import ch.uzh.ifi.seal.changedistiller.treedifferencing.Node;
+import edu.vt.cs.append.FineChangesInMethod;
+import edu.vt.cs.append.JavaExpressionConverter;
+import edu.vt.cs.append.terms.VariableTypeBindingTerm;
 import org.eclipse.jdt.core.dom.*;
 import org.json.simple.JSONObject;
 
+import java.io.File;
 import java.util.*;
 
 public class GeneratePattern {
@@ -11,6 +22,10 @@ public class GeneratePattern {
     private Map<String, String> pattern = new HashMap<>();
     private Set<String> methodtype = new HashSet<>();
     private List<CodePattern> patternlist = new ArrayList<CodePattern>();
+    private File left;
+    private File right;
+    private CompilationUnit lcu;
+    private CompilationUnit rcu;
 
     GeneratePattern(ASTNode node, SourceCodeChange change){
         JSONObject value = new JSONObject();
@@ -22,6 +37,12 @@ public class GeneratePattern {
 
     GeneratePattern(){
         this.methodtype.add("public static final javax.crypto.Cipher getInstance");
+    }
+    GeneratePattern(File left, File right, CompilationUnit lcu, CompilationUnit rcu){
+        this.left = left;
+        this.right = right;
+        this.lcu = lcu;
+        this.rcu= rcu;
     }
 
     public Map<String, String> getPattern()
@@ -46,7 +67,7 @@ public class GeneratePattern {
 //                return super.visit(node);
 //            }
 
-//            @Override
+            //            @Override
 //            public boolean visit(StringLiteral node) {
 //                System.out.println(node);
 //                return super.visit(node);
@@ -58,20 +79,121 @@ public class GeneratePattern {
 //                return super.visit(node);
 //            }
             @Override
-            public boolean visit(MethodInvocation node){
+            public boolean visit(MethodInvocation node) {
                 //&& node.arguments().get(0).toString().equals("\"MD5\"")
                 //if (methodtype.contains(node.resolveMethodBinding().toString())) {
-                System.out.println("method invocation++:"+ node);
-                System.out.println("MI_information: "+ node.resolveMethodBinding());
+                System.out.println("method invocation++:" + node);
+                System.out.println("MI_information: " + node.resolveMethodBinding());
                 patternlist.add(new ParameterPattern(node.resolveMethodBinding().toString()));
                 //pattern.put(str_list, null);
-                //targetnode.add(node);
-               // }
+                targetnode.add(node);
+                // }
                 return super.visit(node);
-
             }
         });
     }
 
+    public void Compare(File left, File right, CompilationUnit lcu, CompilationUnit rcu){
+            FileDistiller distiller = ChangeDistiller.createFileDistiller();
+            FileDistiller.setIgnoreComments();
+            try {
+                distiller.extractClassifiedSourceCodeChanges(left, right);
+            } catch (Exception e) {
+                System.out.println("warning" + e.getMessage());
+            }
 
+            //get the statement of changedistiller and oldfrang, newfrang
+            List<SourceCodeChange> changes = distiller.getSourceCodeChanges();//the size of changes=1, why it is list type??
+            List<SourceRange> srlist = new ArrayList<>();
+            List<SourceRange> newsrlist  = new ArrayList<>();
+            if (changes != null) {
+                for (SourceCodeChange change : changes) {
+                    System.out.println("change operation"+ change);
+                    FineChangesInMethod fc = (FineChangesInMethod) change;
+                    List<SourceCodeChange> changelist =fc.getChanges();
+                    for (SourceCodeChange scc : fc.getChanges()) {
+                        if(scc instanceof Update) {
+                            srlist.add(scc.getChangedEntity().getSourceRange());
+                            System.out.println(scc.getChangedEntity().getSourceRange());
+                            System.out.println(((Update) scc).getNewEntity().getSourceRange()); //get frange
+                            newsrlist.add(((Update) scc).getNewEntity().getSourceRange());
+                        }
+
+                        if(scc instanceof Insert){
+                            System.out.println(scc.getChangedEntity().getSourceRange());
+                            //System.out.println(((Insert) scc).getParentEntity().getSourceRange());
+                            newsrlist.add(((Insert) scc).getChangedEntity().getSourceRange());
+                        }
+
+                        if(scc instanceof Delete) {
+                            srlist.add(scc.getChangedEntity().getSourceRange());
+                            System.out.println(scc.getChangedEntity().getSourceRange());
+//                        System.out.println(((Delete) scc).getNewEntity().getSourceRange());
+                        }
+                    }
+                }
+            }
+
+            //get the AST node from list and filter by API
+            List<ASTNode> ltargetnode =new ArrayList<>();
+            List<ASTNode> rtargetnode = new ArrayList<>();
+            GeneratePattern pa1 = new GeneratePattern();
+            for (SourceRange l: srlist) {
+                ASTNode ltmpNode =NodeFinder.perform(lcu.getRoot(),l.getStart(),l.getEnd()-l.getStart());
+                pa1.VisitTarget(ltmpNode,ltargetnode);
+            }
+            for (SourceRange r: newsrlist){
+                ASTNode rtmpNode =NodeFinder.perform(rcu.getRoot(),r.getStart(),r.getEnd()-r.getStart());
+                pa1.VisitTarget(rtmpNode,rtargetnode);
+            }
+
+            //TODO : 遍历提出堆成的node
+            JavaExpressionConverter leftConverter = new JavaExpressionConverter();
+            ltargetnode.get(0).accept(leftConverter);
+            Node leftRoot = leftConverter.getRoot();
+//        JavaExpressionConverter.markSubStmts(leftRoot, ltargetnode);
+            JavaExpressionConverter rightConverter = new JavaExpressionConverter();
+            rtargetnode.get(0).accept(rightConverter);
+            Node rightRoot = rightConverter.getRoot();
+            //get the low level-differcing
+            Enumeration<Node> lEnum = leftRoot.breadthFirstEnumeration();
+            Enumeration<Node> rEnum = rightRoot.breadthFirstEnumeration();
+            Map<String, String> lkeymapping =new HashMap<String, String>();
+            Map<String, String> rkeymapping =new HashMap<String, String>();
+            //get typeinformation
+            for(String key: leftConverter.variableTypeMap.keySet()){
+                String[] split = key.split("\\+");
+                lkeymapping.put(split[0], key);
+            }
+            for(String key: rightConverter.variableTypeMap.keySet()){
+                String[] split = key.split("\\+");
+                rkeymapping.put(split[0], key);
+            }
+
+            while(lEnum.hasMoreElements() && rEnum.hasMoreElements()) {
+                Node lcurNode = lEnum.nextElement();
+                Node rcurNode = rEnum.nextElement();
+                //       System.out.println("leftdifferencing:"+lcurNode.getEntity());
+                //get parameter differnece and binding information
+
+                Map<String, String> operation = pa1.getPattern();
+                if(!lcurNode.getValue().equals(rcurNode.getValue())) {
+                    System.out.println("leftdifferencing:"+lcurNode.getValue()); //md5
+                    VariableTypeBindingTerm vtbtobj = (VariableTypeBindingTerm) lcurNode.getUserObject();
+                    System.out.println("ABSTRACT NAME: " + vtbtobj.getAbstractVariableName());
+                    if (vtbtobj.getAbstractVariableName().startsWith("v")) {
+                        //new ParameterPattern();
+                    }
+                    System.out.println("rightdiffernt:"+ rcurNode.getValue()); //sha256
+                    VariableTypeBindingTerm vtbt = leftConverter.variableTypeMap.get(lkeymapping.get(lcurNode.getValue()));
+                    System.out.println(vtbt);
+                }
+
+            }
+            System.out.println();
+
+        }
 }
+
+
+
--- src/com/changedistiller/test/Main.java
+++ src/com/changedistiller/test/Main.java
@@ -30,100 +30,10 @@ public class Main {
         FiletoAST rightast = new FiletoAST(rsourcepath, right, rfilename);
         CompilationUnit lcu = leftast.getComplicationUnit();
         CompilationUnit rcu = rightast.getComplicationUnit();
+        GeneratePattern gp = new GeneratePattern();
+        gp.Compare(left, right, lcu, rcu);
 
-        FileDistiller distiller = ChangeDistiller.createFileDistiller();
-        FileDistiller.setIgnoreComments();
-        try {
-            distiller.extractClassifiedSourceCodeChanges(left, right);
-        } catch (Exception e) {
-            System.out.println("warning" + e.getMessage());
-        }
 
-        //get the statement of changedistiller and oldfrang, newfrang
-        List<SourceCodeChange> changes = distiller.getSourceCodeChanges();//the size of changes=1, why it is list type??
-        List<SourceRange> srlist = new ArrayList<>();
-        List<SourceRange> newsrlist  = new ArrayList<>();
-        if (changes != null) {
-            for (SourceCodeChange change : changes) {
-                System.out.println("change operation"+ change);
-                FineChangesInMethod fc = (FineChangesInMethod) change;
-                List<SourceCodeChange> changelist =fc.getChanges();
-                for (SourceCodeChange scc : fc.getChanges()) {
-                    if(scc instanceof Update) {
-                        srlist.add(scc.getChangedEntity().getSourceRange());
-                        System.out.println(scc.getChangedEntity().getSourceRange());
-                        System.out.println(((Update) scc).getNewEntity().getSourceRange()); //get frange
-                        newsrlist.add(((Update) scc).getNewEntity().getSourceRange());
-                    }
-
-                    if(scc instanceof Insert){
-                       System.out.println(scc.getChangedEntity().getSourceRange());
-                       //System.out.println(((Insert) scc).getParentEntity().getSourceRange());
-                       newsrlist.add(((Insert) scc).getChangedEntity().getSourceRange());
-                    }
-
-                    if(scc instanceof Delete) {
-                        srlist.add(scc.getChangedEntity().getSourceRange());
-                        System.out.println(scc.getChangedEntity().getSourceRange());
-//                        System.out.println(((Delete) scc).getNewEntity().getSourceRange());
-                    }
-                }
-            }
-        }
-
-        //get the AST node from list and filter by API
-        List<ASTNode> ltargetnode =new ArrayList<>();
-        List<ASTNode> rtargetnode = new ArrayList<>();
-        GeneratePattern pa1 = new GeneratePattern();
-        for (SourceRange l: srlist) {
-            ASTNode ltmpNode =NodeFinder.perform(lcu.getRoot(),l.getStart(),l.getEnd()-l.getStart());
-            pa1.VisitTarget(ltmpNode,ltargetnode);
-        }
-        for (SourceRange r: newsrlist){
-            ASTNode rtmpNode =NodeFinder.perform(rcu.getRoot(),r.getStart(),r.getEnd()-r.getStart());
-            pa1.VisitTarget(rtmpNode,rtargetnode);
-        }
-
-        //TODO : 遍历提出堆成的node
-        JavaExpressionConverter leftConverter = new JavaExpressionConverter();
-        ltargetnode.get(0).accept(leftConverter);
-        Node leftRoot = leftConverter.getRoot();
-//        JavaExpressionConverter.markSubStmts(leftRoot, ltargetnode);
-
-        JavaExpressionConverter rightConverter = new JavaExpressionConverter();
-        rtargetnode.get(0).accept(rightConverter);
-        Node rightRoot = rightConverter.getRoot();
-
-        //get the low level-differcing
-        Enumeration<Node> lEnum = leftRoot.breadthFirstEnumeration();
-        Enumeration<Node> rEnum = rightRoot.breadthFirstEnumeration();
-        Map<String, String> lkeymapping =new HashMap<String, String>();
-        Map<String, String> rkeymapping =new HashMap<String, String>();
-
-        //get typeinformation
-        for(String key: leftConverter.variableTypeMap.keySet()){
-            String[] split = key.split("\\+");
-            lkeymapping.put(split[0], key);
-        }
-        for(String key: rightConverter.variableTypeMap.keySet()){
-            String[] split = key.split("\\+");
-            rkeymapping.put(split[0], key);
-        }
-
-        while(lEnum.hasMoreElements() && rEnum.hasMoreElements()) {
-            Node lcurNode = lEnum.nextElement();
-            Node rcurNode = rEnum.nextElement();
-            //get parameter differnece and binding information
-            Map<String, String> operation = pa1.getPattern();
-            if(!lcurNode.getValue().equals(rcurNode.getValue())) {
-                System.out.println("leftdifferencing:"+lcurNode.getValue());
-                System.out.println("rightdiffernt:"+ rcurNode.getValue());
-                VariableTypeBindingTerm vtbt = leftConverter.variableTypeMap.get(lkeymapping.get(lcurNode.getValue()));
-                System.out.println(vtbt);
-            }
-
-        }
-        System.out.println();
     }
 }
 
