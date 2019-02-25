6357f7c4b5bf2b557c91c556dc407c59dc5c993c
NEU_Zhangy <alice7neusoft1207@gmail.com>
2019-02-25 14:10:59 -0500

    bingingtype -> bindingtype

--- src/com/changedistiller/test/ParameterPattern.java
+++ src/com/changedistiller/test/ParameterPattern.java
@@ -2,14 +2,14 @@ package com.changedistiller.test;
 
 
 public class ParameterPattern implements CodePattern {
-    private String bingingType = new String();
+    private String bindingType = new String();
 
     ParameterPattern(String str){
-        bingingType = str;
+        bindingType = str;
     }
 
     public String getBingingType() {
-        return bingingType;
+        return bindingType;
     }
 
 
